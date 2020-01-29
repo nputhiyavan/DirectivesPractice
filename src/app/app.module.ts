1c2acdcb07d9dc6faef05f00970a767a66a34862
Puthiyavan <NPuthiyavan@ashleyfurniture.com>
2020-01-29 21:46:38 +0530

    Event Binding Excercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DirectiveComponent } from './directive/directive.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectiveComponent
+    DirectiveComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule
